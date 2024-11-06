0cdd49bf99f5febf734c4b53846efc92082a121b
cohys7 <cohys7@gmail.com>
2024-11-07 04:51:40 +0900

    테스트 성공 시키기: 0으로 나누는 경우 예외 처리 추가

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -13,6 +13,9 @@ public class StringCalculator {
             return Integer.parseInt(numbers[0]) * Integer.parseInt(numbers[1]);
         } else if (expression.contains("/")) {
             String[] numbers = expression.split("/");
+            if (numbers[1].equals("0")) {
+                throw new IllegalArgumentException("Divide by zero");
+            }
             return Integer.parseInt(numbers[0]) / Integer.parseInt(numbers[1]);
         }
         throw new IllegalArgumentException("Unsupported operation");
