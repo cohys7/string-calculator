25e1bbfd48886f947af0d0334364659ee3725551
cohys7 <cohys7@gmail.com>
2024-11-07 05:01:36 +0900

    테스트 성공 시키기: 숫자 대신 문자가 들어오는 경우 예외처리 추가

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -4,23 +4,31 @@ public class StringCalculator {
     public int calculate(String expression) {
         if (expression.contains("+")) {
             String[] numbers = expression.split("\\+");
-            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+            return parseNumber(numbers[0]) + parseNumber(numbers[1]);
         }
         if (expression.contains("-")) {
             String[] numbers = expression.split("-");
-            return Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]);
+            return parseNumber(numbers[0]) - parseNumber(numbers[1]);
         }
         if (expression.contains("*")) {
             String[] numbers = expression.split("\\*");
-            return Integer.parseInt(numbers[0]) * Integer.parseInt(numbers[1]);
+            return parseNumber(numbers[0]) * parseNumber(numbers[1]);
         }
         if (expression.contains("/")) {
             String[] numbers = expression.split("/");
             if (numbers[1].equals("0")) {
                 throw new IllegalArgumentException("Divide by zero");
             }
-            return Integer.parseInt(numbers[0]) / Integer.parseInt(numbers[1]);
+            return parseNumber(numbers[0]) / parseNumber(numbers[1]);
         }
         throw new IllegalArgumentException("Unsupported operation");
+    }
+
+    private Integer parseNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid number");
+        }
     }
 }
